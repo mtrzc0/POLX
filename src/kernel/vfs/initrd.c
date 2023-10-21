5e0c55f2e671c6c7eace15e3c66185237f707969
Tryton77 <segfault52@protonmail.com>
2023-10-21 22:10:24 +0200

    Added missing operations to '/' vfs_node

--- src/kernel/vfs/initrd.c
+++ src/kernel/vfs/initrd.c
@@ -20,6 +20,8 @@ void initrd_load(uintptr_t start_of_initrd)
 	namei_mount(tree_root, initrd_root);
 	
 	/* Set previous op values of "/" node*/
+	tree_root->op.open    = &ramfs_open;
+	tree_root->op.close   = &ramfs_close;
 	tree_root->op.touch   = &ramfs_touch;
 	tree_root->op.remove  = &ramfs_remove;
 	tree_root->op.mkdir   = &ramfs_mkdir;
